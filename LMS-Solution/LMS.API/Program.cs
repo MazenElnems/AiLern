dc66b54757632a0c5d5fbeaa6e4982490f3e190e
Mazen Elnems <mazenelnems22@gmail.com>
2025-10-18 19:27:13 +0300

    Add ServicesCollection Extenstions
    - add service collection extenstions in Core Project
    - add service collection extenstions in Infrastructrure Project

--- LMS-Solution/LMS.API/Program.cs
+++ LMS-Solution/LMS.API/Program.cs
@@ -1,69 +1,21 @@
-using LMS.API.ConfigurationOptions;
-using LMS.Core.ConfigurationOptions;
-using LMS.Core.Domain.Entities;
-using LMS.Core.Services;
-using LMS.Core.Services.Interfaces;
-using LMS.Infrastructure.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using LMS.Core.Extensions;
+using LMS.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// swagger & open api
 builder.Services.AddOpenApi();
-
-// Custom Services
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ICookieService, CookieService>();
-
-// swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// DbConext
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+// Add Infrastructure Services 
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
-// Identity
-builder.Services.AddIdentity<ApplicationUser, IdentityRole<int>>(options =>
-{
-    options.Password.RequireUppercase = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequireDigit = true;
-}).AddEntityFrameworkStores<AppDbContext>();
-
-// JWT 
-builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JWT"));
-builder.Services.Configure<RefreshTokenSettings>(builder.Configuration.GetSection("RefreshTokenSettings"));
-var jwt = builder.Configuration.GetSection("JWT").Get<JwtOptions>();
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(o =>
-    {
-        o.RequireHttpsMetadata = true;
-        o.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidIssuer = jwt?.Issuer,
-            ValidAudience = jwt?.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt?.Key)),
-            ClockSkew = TimeSpan.Zero
-        };
-    });
+// Add Core Services
+builder.Services.AddRequiredServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -76,6 +28,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHsts();
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
