945dc8de5278deebce789ddb645ded15034bcad4
Mazen Elnems <mazenelnems22@gmail.com>
2025-10-18 17:18:46 +0300

    Add course cru ds (#2)
    * Add course entity
    
    - add course entity
    - add student eentity
    - add instructor entity
    - add admin entity
    - add the default course configurations.
    
    * Add Course Configuration
    
    - Add Entities Configuration
    - Add Migration and Update Database
    
    ---------
    
    Co-authored-by: Ahmed Albatrawy <albatrawyahmed@gmail.com>

---
+++ LMS-Solution/LMS.Core/Domain/Entities/EntitiesConfiguration/InstructorEntityTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Core.Domain.Entities.EntitiesConfiguration
+{
+    public class InstructorEntityTypeConfiguration : IEntityTypeConfiguration<Instructor>
+    {
+        public void Configure(EntityTypeBuilder<Instructor> builder)
+        {
+
+        }
+    }
+}
---
+++ LMS-Solution/LMS.Infrastructure/Migrations/20251016132856_Add_Course_Configurations.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Course_Configurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "VARCHAR(10)", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    Description = table.Column<string>(type: "NVARCHAR(MAX)", nullable: true),
+                    CourseStatus = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    ApprovedDate = table.Column<DateTime>(type: "DATETIME2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "DATETIME2", nullable: false),
+                    InstructorId = table.Column<int>(type: "int", nullable: false),
+                    Approvedby = table.Column<int>(type: "int", nullable: true),
+                    SectionCourseId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Courses_AspNetUsers_Approvedby",
+                        column: x => x.Approvedby,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Courses_AspNetUsers_InstructorId",
+                        column: x => x.InstructorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_Courses_SectionCourseId",
+                        column: x => x.SectionCourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Approvedby",
+                table: "Courses",
+                column: "Approvedby");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Code",
+                table: "Courses",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_InstructorId",
+                table: "Courses",
+                column: "InstructorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Name",
+                table: "Courses",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_SectionCourseId",
+                table: "Courses",
+                column: "SectionCourseId",
+                unique: true,
+                filter: "[SectionCourseId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "AspNetUsers");
+        }
+    }
+}
