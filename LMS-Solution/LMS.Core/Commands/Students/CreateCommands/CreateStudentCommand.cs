3b47d964b339d55a9e33ad52a7db3c67e9c955af
Ahmed Albatrawy <albatrawyahmed@gmail.com>
2025-11-10 17:08:49 +0200

    Add Student Register Endpoint : -  Add StudentId Property To Student Entity and Configure it -  Create New Migration To Update Database -  Create Command and Command Handler For Create Student -  Create DuplicatedUserException -  Add GetStudentByStudentId In UserRepository

---
+++ LMS-Solution/LMS.Core/Commands/Students/CreateCommands/CreateStudentCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LMS.Core.Commands.Students.CreateCommands
+{
+    public class CreateStudentCommand : IRequest
+    {
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public int StudentId { get; set; }
+    }
+}
