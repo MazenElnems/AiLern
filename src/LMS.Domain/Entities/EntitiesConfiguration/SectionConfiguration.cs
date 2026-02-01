86e148341a1afed3cc1f74e1b1aabcaca67dac40
Mazen Elnems <mazenelnems22@gmail.com>
2026-02-01 14:26:20 +0200

    add Sections and MatrialFile Entities
    add MatrialFile Configuration
    add Section Configuration

---
+++ src/LMS.Domain/Entities/EntitiesConfiguration/SectionConfiguration.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Domain.Entities.EntitiesConfiguration;
+
+public class SectionConfiguration : IEntityTypeConfiguration<Section>
+{
+    public void Configure(EntityTypeBuilder<Section> builder)
+    {
+        builder.ToTable("Sections");
+
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.Title)
+            .HasColumnType("NVARCHAR(200)")
+            .IsRequired();
+
+        builder.Property(s => s.SectionNumber)
+            .HasColumnType("INT")
+            .IsRequired();
+
+        builder.HasOne(s => s.Course)
+            .WithMany()
+            .HasForeignKey(s => s.CourseId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(s => s.MaterialFiles)
+            .WithOne(mf => mf.Section)
+            .HasForeignKey(mf => mf.SectionId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
