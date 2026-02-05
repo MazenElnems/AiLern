6e82728e32f22742e1d2bfd5e06be4e2e5ffb15d
Mazen Elnems <mazenelnems22@gmail.com>
2026-02-05 17:19:13 +0200

    Add DB initialization and seeding on app startup
    - add IDbInitializer and DbInitializer to apply migrations and seed default roles and users (admin, instructor, student) during application startup.
    - add config for default user credentials.
    - add extension for IHost to trigger DB initialization.

---
+++ src/LMS.API/Extensions/DbIntializationExtension.cs
@@ -0,0 +1,13 @@
+﻿using LMS.Domain.Interfaces;
+
+namespace LMS.API.Extensions;
+
+public static class DbIntializationExtension
+{
+    public static async Task InitializeDatabaseAsync(this IHost app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        await dbInitializer.InitializeAsync();
+    }
+}
---
+++ src/LMS.Domain/Interfaces/IDbInitializer.cs
@@ -0,0 +1,6 @@
+﻿namespace LMS.Domain.Interfaces;
+
+public interface IDbInitializer
+{
+    Task InitializeAsync();
+}
