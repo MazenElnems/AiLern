a2319e182afa319420e1a3a6eee822ec6cfa1f72
Mazen Elnems <mazenelnems22@gmail.com>
2026-03-23 17:28:58 +0200

    refactor DomainErrors and AttemptsController

--- src/LMS.API/Controllers/AttemptsController.cs
+++ src/LMS.API/Controllers/AttemptsController.cs
@@ -14,8 +14,6 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace LMS.API.Controllers;
 
-namespace LMS.API.Controllers
-{
 [Route("api/[controller]")]
 [ApiController]
 public class AttemptsController : ApiBaseController
@@ -27,10 +25,7 @@ public class AttemptsController : ApiBaseController
         this._mediator = mediator;
             _mediator = mediator;
     }
-    [HttpGet("{id}/student")]
 
-        [HttpPost("/api/quizzes/{quizId}/[controller]")]
-    [Authorize(Roles = UserRoles.Student)]
     [SwaggerOperation(Summary = "Get attempt", Description = "Get an attempt by id.")]
     [SwaggerResponse(StatusCodes.Status200OK, "Attempt retrieved successfully.", typeof(ApiResponse))]
     [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid request.", typeof(ApiResponse))]
@@ -38,38 +33,48 @@ public class AttemptsController : ApiBaseController
     [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden.", typeof(ApiResponse))]
     [SwaggerResponse(StatusCodes.Status404NotFound, "quiz not found.", typeof(ApiResponse))]
     [SwaggerResponse(StatusCodes.Status500InternalServerError, "Server error.", typeof(ApiResponse))]
+    [HttpGet("{id}/student")]
+    [Authorize(Roles = UserRoles.Student)]
     public async Task<ActionResult<ApiResponse>> GetAttemptByIdForStudent(Guid id, [FromQuery] GetAttemptByIdForStudentQuery query)
-        public async Task<ActionResult<ApiResponse>> Create([FromRoute] Guid quizId)
     {
         query.Id = id;
         var result = await _mediator.Send(query);
-            var command = new CreateAttemptCommand(quizId);
-            var result = await _mediator.Send(command);
         return HandleResponse(this, result);
     }
+
+    [Authorize(Roles = UserRoles.Student)]
+    [HttpPost("/api/quizzes/{quizId}/[controller]")]
+    public async Task<ActionResult<ApiResponse>> Create([FromRoute] Guid quizId)
+    {
+        var command = new CreateAttemptCommand(quizId);
+        var result = await _mediator.Send(command);
+        return HandleResponse(this, result);
+    }
+    [SwaggerOperation(Summary = "Get attempt", Description = "Get an attempt by id.")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Attempt retrieved successfully.", typeof(ApiResponse))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid request.", typeof(ApiResponse))]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized, "Unauthorized.", typeof(ApiResponse))]
+    [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden.", typeof(ApiResponse))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "quiz not found.", typeof(ApiResponse))]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "Server error.", typeof(ApiResponse))]
     [HttpGet("{id}/instrudcor")]
     [Authorize(Roles = UserRoles.Instructor)]
-    [SwaggerOperation(Summary = "Get attempt", Description = "Get an attempt by id.")]
-    [SwaggerResponse(StatusCodes.Status200OK, "Attempt retrieved successfully.", typeof(ApiResponse))]
-    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid request.", typeof(ApiResponse))]
-    [SwaggerResponse(StatusCodes.Status401Unauthorized, "Unauthorized.", typeof(ApiResponse))]
-    [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden.", typeof(ApiResponse))]
-    [SwaggerResponse(StatusCodes.Status404NotFound, "quiz not found.", typeof(ApiResponse))]
-    [SwaggerResponse(StatusCodes.Status500InternalServerError, "Server error.", typeof(ApiResponse))]
     public async Task<ActionResult<ApiResponse>> GetAttemptByIdForInstructor(Guid id, [FromQuery] GetAttemptByIdForInstructorQuery query)
-
-        [HttpPut("{attemptId}")]
-        [Authorize(UserRoles.Student)]
-        public async Task<ActionResult<ApiResponse>> Submit([FromRoute] Guid attemptId)
     {
         query.Id = id;
         var result = await _mediator.Send(query);
-            var command = new SubmitAttemptCommand(attemptId);
-            var result = await _mediator.Send(command);
         return HandleResponse(this, result);
     }
-    [HttpPut("{id}/grade")]
-    [Authorize(Roles = UserRoles.Instructor)]
+
+    [HttpPut("{attemptId}")]
+    [Authorize(UserRoles.Student)]
+    public async Task<ActionResult<ApiResponse>> Submit([FromRoute] Guid attemptId)
+    {
+        var command = new SubmitAttemptCommand(attemptId);
+        var result = await _mediator.Send(command);
+        return HandleResponse(this, result);
+    }
+
     [SwaggerOperation(Summary = "Put attempt", Description = "Put an attempt.")]
     [SwaggerResponse(StatusCodes.Status200OK, "Attempt retrieved successfully.", typeof(ApiResponse))]
     [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid request.", typeof(ApiResponse))]
@@ -77,21 +82,21 @@ public class AttemptsController : ApiBaseController
     [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden.", typeof(ApiResponse))]
     [SwaggerResponse(StatusCodes.Status404NotFound, "quiz not found.", typeof(ApiResponse))]
     [SwaggerResponse(StatusCodes.Status500InternalServerError, "Server error.", typeof(ApiResponse))]
+    [HttpPut("{id}/grade")]
+    [Authorize(Roles = UserRoles.Instructor)]
     public async Task<ActionResult<ApiResponse>> GradeSubmission(Guid id, [FromBody] GradeSubmissionCommand command)
-
-        [HttpPost("{attemptId}/save")]
-        [Authorize(Roles = UserRoles.Student)]
-        public async Task<ActionResult<ApiResponse>> Save([FromRoute] Guid attemptId, [FromBody] SaveAttemptCommand command)
-        {
-            command.AttemptId = attemptId;
-            var result = await _mediator.Send(command);
-            return HandleResponse(this, result);
-        }
-
+    {
         var result = await _mediator.Send(command);
         return HandleResponse(this, result);
     }
 
-
+    [HttpPost("{attemptId}/save")]
+    [Authorize(Roles = UserRoles.Student)]
+    public async Task<ActionResult<ApiResponse>> Save([FromRoute] Guid attemptId, [FromBody] SaveAttemptCommand command)
+    {
+        command.AttemptId = attemptId;
+        var result = await _mediator.Send(command);
+        return HandleResponse(this, result);
+    }
 }
 
--- src/LMS.Domain/Errors/DomainErrors.cs
+++ src/LMS.Domain/Errors/DomainErrors.cs
@@ -117,18 +117,15 @@ public static class DomainErrors
 
         public static Error InvalidQuestion(Guid questionId) =>
             Common.Validation("Attempt.InvalidQuestion", $"Question with ID {questionId} does not belong to this attempt.");
-    }
-    public static class Attempt
-    {
-        public static Error NotFound(Guid id) =>
-            Common.NotFound("Attempt", id.ToString());
+
+        public static Error StillInProgress =>
+            Common.BusinessRule("Attempt.StillInProgress", "The attempt is still in progress and cannot be graded.");
 
         public static Error NotOwned =>
             Common.Forbidden("You do not have permission to access this attempt.");
+
         public static Error NotPublished =>
             Common.BusinessRule("Quiz.NotPublished", "Quiz is not published.");
-        public static Error StillInProgress =>
-            Common.BusinessRule("Attempt.StillInProgress", "The attempt is still in progress and cannot be graded.");
 
     }
 
