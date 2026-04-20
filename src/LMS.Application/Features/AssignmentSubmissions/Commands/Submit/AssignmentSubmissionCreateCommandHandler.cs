feb93ead768da220f22b23217ba95d52b7d3fbf8
Ahmed Albatrawy <albatrawyahmed@gmail.com>
2026-04-20 14:12:48 +0200

    Complete the Resolving Conflect

--- src/LMS.Application/Features/AssignmentSubmissions/Commands/Submit/AssignmentSubmissionCreateCommandHandler.cs
+++ src/LMS.Application/Features/AssignmentSubmissions/Commands/Submit/AssignmentSubmissionCreateCommandHandler.cs
@@ -68,7 +68,7 @@ public class AssignmentSubmissionCreateCommandHandler : IRequestHandler<Assignme
         List<string> keys = new();
         foreach (var file in request.FileMetaData)
         {
-            var key = $"courses/{course.Id}/assignments/{assignment.Id}/submissions/{submission.Id}/{Guid.NewGuid()}.{file.FileName.Split('.').Last()}";
+            var key = $"courses/{course.Id}/assignments/{assignment.Id}/submissions/{user.Id}/{Guid.NewGuid()}.{file.FileName.Split('.').Last()}";
             var url = await _wasabiService.GeneratePresignedUploadUrlAsync(key, file.ContentType, 15);
             fileUrls.Add(url);
 
