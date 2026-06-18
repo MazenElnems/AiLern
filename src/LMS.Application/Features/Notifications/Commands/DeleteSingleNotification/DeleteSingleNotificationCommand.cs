8b3f1046e9b74ba52b9b53fc79b0a736b1102c0c
Mazen Elnems <mazenelnems22@gmail.com>
2026-06-18 19:02:58 +0300

    Add endpoint to delete a single user notification
    Implemented API endpoint and command handler to allow users to delete a specific notification by ID. Includes authorization, Swagger docs, and proper result handling.

---
+++ src/LMS.Application/Features/Notifications/Commands/DeleteSingleNotification/DeleteSingleNotificationCommand.cs
@@ -0,0 +1,6 @@
+﻿using LMS.Application.Common.Results;
+using MediatR;
+
+namespace LMS.Application.Features.Notifications.Commands.DeleteSingleNotification;
+
+public record DeleteSingleNotificationCommand(Guid NotificationId) : IRequest<Result>;
